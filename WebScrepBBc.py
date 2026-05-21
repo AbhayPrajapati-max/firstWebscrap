10ef76c516c7ee9bd747a3c69a8d52164c593093
AbhayPrajapati-max <prajapatiabhay421@gmail.com>
2026-05-21 14:37:38 +0530

    Update WebScrepBBc.py

--- WebScrepBBc.py
+++ WebScrepBBc.py
@@ -10,10 +10,10 @@ title=soup.title.text
 print(f"this is {title}")
 name=soup.find_all("h2")
 for h in name:
- print(h)
  text=h.get_text(strip=True)
  print(f'this first text \n{text}')
  if text:
   print(f'this if Text \n{text}')
+ 
   with open('bbcnew.txt','a',encoding='utf-8') as f:
    f.write(text+'\n')
